fefe4e2697fb3819675d88b2e1da7fae54b2ec4c
Audrius Petrauskas <audrius.j.petrauskas@gmail.com>
2022-03-01 17:34:34 +0000

    Add renamed contract

---
+++ Services/Contracts/GameInstanceContract.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Services.Contracts
+{
+    public class GameInstanceContract
+    {
+        public Guid gameId;
+
+        public string levelid;
+
+        public List<EventContract> levelEvents;
+
+        public List<EventContract> usedEvents;
+
+        public int mistakes;
+
+        public List<EventContract> mistakenEvents;
+    }
+}
